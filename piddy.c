2ef95235d70330d6246f0f666866e383b2b1cde0
eugene and cicelo <ekiukkim90@gmail.com>
2023-07-18 12:25:53 -0700

    simpleshellk

---
+++ piddy.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * piddy - forks a child process
+ *
+ * Return: status
+ */
+
+void piddy(void)
+{
+	int status;
+	pid_t child_pros;
+	ppid_t parent_pros;
+
+	return (status);
+}
--- shell.h
+++ shell.h
@@ -1,5 +1,12 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+void piddy(void);
 
 #endif

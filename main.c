e5246ac3772772c19ccd02a84324ef401469dc0d
eugene and cicelo <ekiukkim90@gmail.com>
2023-07-18 12:15:38 -0700

    simpleshellk

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * main - run a simple shell program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,5 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#endif

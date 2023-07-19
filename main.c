f086cf157a2e27833b62b55673f786ff5eb19208
eugene and cicelo <ekiukkim90@gmail.com>
2023-07-19 15:24:23 -0700

    simpleshellk

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 /**
  * main - run a simple shell program
+ * @argc: argument count
+ * @argv: receives the string
  *
  * Return: 0
  */
 void piddy(void);
 
-int main(void)
+int main(int argc,char **argv)
 {
 	return (0);
 }
